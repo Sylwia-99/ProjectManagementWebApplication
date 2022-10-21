d4186c84e85c6b3e8ac01e4dd9b2c640277e37c8
sylwia02.02.1999@interia.pl <sylwia02.02.1999@interia.pl>
2022-10-21 20:54:26 +0200

    Register form has been added

--- frontend/angularProject/src/app/app.module.ts
+++ frontend/angularProject/src/app/app.module.ts
@@ -6,11 +6,17 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavigationModule } from './navigation/navigation.module';
 import { LoginFormComponent } from './login-form/login-form.component';
+import { RegisterFormComponent } from './register-form/register-form.component';
+import { LogoPanelComponent } from './commons/logo-panel/logo-panel.component';
+import { ErrorMessageComponent } from './commons/error-message/error-message.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginFormComponent
+    LoginFormComponent,
+    RegisterFormComponent,
+    LogoPanelComponent,
+    ErrorMessageComponent
   ],
   imports: [
     AppRoutingModule, 
