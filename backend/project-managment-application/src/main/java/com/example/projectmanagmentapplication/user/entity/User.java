86b733030edb14445df58512c58bfbf13ab2754a
Kinga Wrona <kingaewelina@gmail.com>
2022-10-27 18:56:28 +0200

    Created Workspace entity and repository for workspace. Updated User entity.

--- backend/project-managment-application/src/main/java/com/example/projectmanagmentapplication/user/entity/User.java
+++ backend/project-managment-application/src/main/java/com/example/projectmanagmentapplication/user/entity/User.java
@@ -1,14 +1,19 @@
 package com.example.projectmanagmentapplication.user.entity;
 
 import lombok.*;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import javax.persistence.*;
+import java.time.LocalDateTime;
 
 @Entity
 @AllArgsConstructor
 @NoArgsConstructor
 @Data
 @Table(name = "\"user\"")
+@EntityListeners(AuditingEntityListener.class)
 public class User {
     @Id
     @GeneratedValue
@@ -20,4 +25,10 @@ public class User {
     @OneToOne(cascade = CascadeType.MERGE, fetch = FetchType.EAGER)
     @JoinColumn(name = "user_details_id", referencedColumnName = "id")
     private UserDetails userDetails;
+
+    @CreatedDate
+    private LocalDateTime createdAt;
+
+    @LastModifiedDate
+    private LocalDateTime updatedAt;
 }
--- backend/project-managment-application/src/main/java/com/example/projectmanagmentapplication/workspace/entity/Workspace.java
+++ backend/project-managment-application/src/main/java/com/example/projectmanagmentapplication/workspace/entity/Workspace.java
@@ -1,4 +1,33 @@
 package com.example.projectmanagmentapplication.workspace.entity;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@EntityListeners(AuditingEntityListener.class)
 public class Workspace {
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    private String workspaceName;
+
+    @Column(columnDefinition = "text")
+    private String description;
+
+    @CreatedDate
+    private LocalDateTime createdAt;
+
+    @LastModifiedDate
+    private LocalDateTime updatedAt;
 }
