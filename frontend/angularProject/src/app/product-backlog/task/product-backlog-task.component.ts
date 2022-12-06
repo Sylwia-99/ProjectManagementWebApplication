8ccff6290de43828a95fe61871478b4733fc4efd
sylwia02.02.1999@interia.pl <sylwia02.02.1999@interia.pl>
2022-12-06 15:24:02 +0100

    Aded preview task in Sprint view

--- frontend/angularProject/src/app/product-backlog/task/product-backlog-task.component.ts
+++ frontend/angularProject/src/app/product-backlog/task/product-backlog-task.component.ts
@@ -3,7 +3,6 @@ import { MatDialog } from '@angular/material/dialog';
 
 import { DOTS } from 'src/constants/constants.data';
 import { ProductBacklogService } from 'src/data/product-backlog.service';
-import { ViewComponent } from '../modals/task-modal/view/view.component';
 import { Task } from 'src/interfaces/product-backlog';
 
 @Component({
@@ -23,16 +22,15 @@ export class ProductBackolgTaskComponent {
   ) { }
 
   editTask(uuid: string){
-    this.productBacklogService.getTask(uuid).subscribe((task: Task) =>  
-        {
-          this.dialogRef.open(ViewComponent, {
-            width: '60vw',
-            height: '50vh',
-            data: task
-          });
-        }
-    );
-    
-    
+    console.log('TO DO')
+    // this.productBacklogService.getTask(uuid).subscribe((task: Task) =>  
+    //     {
+    //       this.dialogRef.open(ViewComponent, {
+    //         width: '60vw',
+    //         height: '50vh',
+    //         data: task
+    //       });
+    //     }
+    // );
   }
 }
